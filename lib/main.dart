80815db12c76cd7dafc4e93767e1d53f92acf840
Erick Pinzon <65203105+erickpinzon18@users.noreply.github.com>
2024-05-28 17:09:19 -0600

    PS hasta ahi jala

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_circle_color_picker/flutter_circle_color_picker.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,11 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
+
+// class Bluetooth extends StatefulWidget {
+//   @override
+//   _BluetoothState createState() => _BluetoothState();
+// }
 
 class _MyAppState extends State<MyApp> {
   Color _currentColor = Colors.blue;
@@ -25,24 +31,39 @@ class _MyAppState extends State<MyApp> {
           title: const Text(
             'RBG LED App',
             style: TextStyle(
-              color: Color.fromRGBO(217, 217, 217, 1),
-              fontWeight: FontWeight.bold
-            ), // Aquí puedes cambiar el color de la fuente
+                color: Color.fromRGBO(217, 217, 217, 1),
+                fontWeight: FontWeight
+                    .bold), // Aquí puedes cambiar el color de la fuente
           ),
           backgroundColor: Color.fromRGBO(26, 26, 26, 1),
           centerTitle: true,
+          // Agregar dropdown para seleccionar el dispositivo bluetooth
+          actions: <Widget>[
+            DropdownButton(
+              icon: Icon(Icons.bluetooth),
+              items: [
+                DropdownMenuItem(
+                  child: Text('Dispositivo 1'),
+                  value: 1,
+                ),
+              ],
+              onChanged: (value) {
+                print(value);
+                // conectar a ese dispositivo
+              },
+            ),
+          ],
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Center(
-              child: Icon(
-                Icons.lightbulb_outline,
-                color: _currentColor,
-                size: 100,
-              )
-            ),
+                child: Icon(
+              Icons.lightbulb_outline,
+              color: _currentColor,
+              size: 100,
+            )),
             const SizedBox(height: 48),
             Center(
               child: CircleColorPicker(
@@ -60,51 +81,53 @@ class _MyAppState extends State<MyApp> {
             Column(
               children: [
                 // Sliders para cada color
-                    Slider(
-                      value: _currentColor.red.toDouble(),
-                      min: 0,
-                      max: 255,
-                      thumbColor: Colors.red,
-                      activeColor: Colors.red,
-                      onChanged: (value) {
-                        setState(() {
-                          _controller.color = Color.fromRGBO(value.toInt(), _currentColor.green, _currentColor.blue, 1);
-                        });
-                      },
-                    ),
-                    Slider(
-                      value: _currentColor.green.toDouble(),
-                      min: 0,
-                      max: 255,
-                      thumbColor: Colors.green,
-                      activeColor: Colors.green,
-                      onChanged: (value) {
-                        setState(() {
-                          _controller.color = Color.fromRGBO(_currentColor.red, value.toInt(), _currentColor.blue, 1);
-                        });
-                      },
-                    ),
-                    Slider(
-                      value: _currentColor.blue.toDouble(),
-                      min: 0,
-                      max: 255,
-                      thumbColor: Colors.blue,
-                      activeColor: Colors.blue,
-                      onChanged: (value) {
-                        setState(() {
-                          _controller.color = Color.fromRGBO(_currentColor.red, _currentColor.green, value.toInt(), 1);
-                        });
-                      },
-                    ),
+                Slider(
+                  value: _currentColor.red.toDouble(),
+                  min: 0,
+                  max: 255,
+                  thumbColor: Colors.red,
+                  activeColor: Colors.red,
+                  onChanged: (value) {
+                    setState(() {
+                      _controller.color = Color.fromRGBO(value.toInt(),
+                          _currentColor.green, _currentColor.blue, 1);
+                    });
+                  },
+                ),
+                Slider(
+                  value: _currentColor.green.toDouble(),
+                  min: 0,
+                  max: 255,
+                  thumbColor: Colors.green,
+                  activeColor: Colors.green,
+                  onChanged: (value) {
+                    setState(() {
+                      _controller.color = Color.fromRGBO(_currentColor.red,
+                          value.toInt(), _currentColor.blue, 1);
+                    });
+                  },
+                ),
+                Slider(
+                  value: _currentColor.blue.toDouble(),
+                  min: 0,
+                  max: 255,
+                  thumbColor: Colors.blue,
+                  activeColor: Colors.blue,
+                  onChanged: (value) {
+                    setState(() {
+                      _controller.color = Color.fromRGBO(_currentColor.red,
+                          _currentColor.green, value.toInt(), 1);
+                    });
+                  },
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(_switchValue ? 'Apagar' : 'Encender', style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      )
-                    ),
+                    Text(_switchValue ? 'Apagar' : 'Encender',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold)),
                     Switch(
                       value: _switchValue,
                       onChanged: (value) {
